4c6406e2203f776d57388ffd0dfe8a47a80d3e14
Mint <mint.devstudio@gmail.com>
2018-04-16 22:47:46 +1000

    add functionality

--- src/app/drag-and-drop/drag-and-drop.directive.ts
+++ src/app/drag-and-drop/drag-and-drop.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-	selector: '[appDragndrop]'
+	selector: '[appDragAndDrop]'
 })
 export class DragAndDropDirective {
 
 	constructor() { }
+
+	@HostBinding('draggable') draggable = true;
+
+	@HostListener('dragenter', ['$event'])
+	onDragEnter(e) {
+		console.log(e);
+	}
 
 }
